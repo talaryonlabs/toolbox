d762fe5a3710cfd05454b9364cf911a451ad484e
Nico <10356831+ferociousbyte@users.noreply.github.com>
2024-07-31 01:09:41 +0200

    Add IGitHub interface and extend IDirectus documentation.
    Introduced a new IGitHub interface with a basic structure. Enhanced the IDirectus interface by adding XML documentation for its methods, providing detailed explanations of their purposes and parameters.

--- src/Toolbox/Services/Directus/IDirectus.cs
+++ src/Toolbox/Services/Directus/IDirectus.cs
@@ -1,17 +1,56 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Extensions;
 
 namespace Talaryon.Toolbox.Services.Directus;
 
+/// <summary>
+/// Represents the interface for making requests to the Directus API.
+/// </summary>
 public interface IDirectus
 {
+    /// <summary>
+    /// Sends a test request to the Directus API to check if it is online.
+    /// </summary>
+    /// <returns>Returns true if the API responds with "pong", otherwise false.</returns>
     Task<bool> Test();
+
+    /// <summary>
+    /// Returns a request object for retrieving a single item from the Directus API.
+    /// </summary>
+    /// <typeparam name="T">The type of the item.</typeparam>
+    /// <param name="name">The name of the item.</param>
+    /// <returns>An object representing the request for retrieving a single item from the Directus API.</returns>
     IDirectusRequestSingle<T> Single<T>(string name);
+
+    /// <summary>
+    /// Returns a request object for retrieving multiple items from the Directus API.
+    /// </summary>
+    /// <typeparam name="T">The type of the items.</typeparam>
+    /// <param name="name">The name of the items.</param>
+    /// <returns>An object representing the request for retrieving multiple items from the Directus API.</returns>
     IDirectusRequestSingle<T> Select<T>(string name, string? id);
+
+    /// <summary>
+    /// Sends a request to the Directus API to retrieve multiple items of type T.
+    /// </summary>
+    /// <typeparam name="T">The type of items to retrieve.</typeparam>
+    /// <param name="name">The name of the table to retrieve items from.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the response from the API containing an array of items of type T, or null if no items were found.</returns>
     IDirectusRequestMany<T> Many<T>(string name);
 
+    /// <summary>
+    /// Gets the URL of an asset in the Directus API.
+    /// </summary>
+    /// <param name="assetId">The ID of the asset.</param>
+    /// <returns>The URL of the asset.</returns>
     string GetAssetUrl(string? assetId);
+
+    /// <summary>
+    /// Returns the URL of the asset with the specified ID.
+    /// </summary>
+    /// <param name="assetId">The ID of the asset.</param>
+    /// <param name="queryString">The query string to be appended to the URL. It can be used to include additional parameters.</param>
+    /// <returns>The URL of the asset.</returns>
     string GetAssetUrl(string? assetId, QueryString queryString);
 }
 
