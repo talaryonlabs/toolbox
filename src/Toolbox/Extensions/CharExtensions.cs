85b909eae28679decb24ab92b3a9149b39ef01bd
Nico <10356831+ferociousbyte@users.noreply.github.com>
2024-09-25 14:37:21 +0200

    Delete Services.razor page and add NavBar-Logo.light.svg
    Removed the Services.razor component, likely for a refactor or cleanup. Introduced a new SVG image for the navigation bar logo, enhancing the visual elements.

---
+++ src/Toolbox/Extensions/CharExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Talaryon.Toolbox.Extensions;
+
+public static class CharExtensions
+{
+    private static readonly Dictionary<char, string> CharMappings = new()
+    {
+        { 'à', "a" }, { 'å', "a" }, { 'á', "a" }, { 'â', "a" }, { 'ä', "a" },
+        { 'ã', "a" }, { 'å', "a" }, { 'ą', "a" },
+        { 'è', "e" }, { 'é', "e" }, { 'ê', "e" }, { 'ë', "e" }, { 'ę', "e" },
+        { 'ì', "i" }, { 'í', "i" }, { 'î', "i" }, { 'ï', "i" }, { 'ı', "i" },
+        { 'ò', "o" }, { 'ó', "o" }, { 'ô', "o" }, { 'õ', "o" }, { 'ö', "o" }, { 'ø', "o" }, { 'ő', "o" }, { 'ð', "o" },
+        { 'ù', "u" }, { 'ú', "u" }, { 'û', "u" }, { 'ü', "u" }, { 'ŭ', "u" }, { 'ů', "u" },
+        { 'ç', "c" }, { 'ć', "c" }, { 'č', "c" }, { 'ĉ', "c" },
+        { 'ż', "z" }, { 'ź', "z" }, { 'ž', "z" },
+        { 'ś', "s" }, { 'ş', "s" }, { 'š', "s" }, { 'ŝ', "s" },
+        { 'ñ', "n" }, { 'ń', "n" },
+        { 'ý', "y" }, { 'ÿ', "y" },
+        { 'ğ', "g" }, { 'ĝ', "g" },
+        { 'ř', "r" },
+        { 'ł', "l" },
+        { 'đ', "d" },
+        { 'ß', "ss" },
+        { 'þ', "th" },
+        { 'ĥ', "h" },
+        { 'ĵ', "j" }
+    };
+    
+    public static string RemapInternationalCharToAscii(this char c)
+    {
+        return CharMappings.TryGetValue(c, out var mappedValue) ? mappedValue : string.Empty;
+    }
+}
--- src/Toolbox/Extensions/StringExtensions.cs
+++ src/Toolbox/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text;
 
 namespace Talaryon.Toolbox.Extensions;
@@ -20,7 +21,69 @@ public static class StringExtensions
 
     public static string FromBase64String(this string p)
     {
-        var bytes= Convert.FromBase64String(p);
+        var bytes = Convert.FromBase64String(p);
         return Encoding.UTF8.GetString(bytes);
+    }
+
+    public static string SlugifyString(this string p, int maxLength = 0)
+    {
+        var normalizedString = p
+            // Make lowercase
+            .ToLowerInvariant()
+            // Normalize the text
+            .Normalize(NormalizationForm.FormD);
+
+        var stringBuilder = new StringBuilder();
+        var stringLength = normalizedString.Length;
+        var prevdash = false;
+        var trueLength = 0;
+
+        char c;
+
+        for (var i = 0; i < stringLength; i++)
+        {
+            c = normalizedString[i];
+
+            switch (CharUnicodeInfo.GetUnicodeCategory(c))
+            {
+                // Check if the character is a letter or a digit if the character is a
+                // international character remap it to an ascii valid character
+                case UnicodeCategory.LowercaseLetter:
+                case UnicodeCategory.UppercaseLetter:
+                case UnicodeCategory.DecimalDigitNumber:
+                    if (c < 128)
+                        stringBuilder.Append(c);
+                    else
+                        stringBuilder.Append(c.RemapInternationalCharToAscii());
+
+                    prevdash = false;
+                    trueLength = stringBuilder.Length;
+                    break;
+
+                // Check if the character is to be replaced by a hyphen but only if the last character wasn't
+                case UnicodeCategory.SpaceSeparator:
+                case UnicodeCategory.ConnectorPunctuation:
+                case UnicodeCategory.DashPunctuation:
+                case UnicodeCategory.OtherPunctuation:
+                case UnicodeCategory.MathSymbol:
+                    if (!prevdash)
+                    {
+                        stringBuilder.Append('-');
+                        prevdash = true;
+                        trueLength = stringBuilder.Length;
+                    }
+                    break;
+            }
+
+            // If we are at max length, stop parsing
+            if (maxLength > 0 && trueLength >= maxLength)
+                break;
+        }
+
+        // Trim excess hyphens
+        var result = stringBuilder.ToString().Trim('-');
+
+        // Remove any excess character to meet maxlength criteria
+        return maxLength <= 0 || result.Length <= maxLength ? result : result.Substring(0, maxLength);
     }
 }
