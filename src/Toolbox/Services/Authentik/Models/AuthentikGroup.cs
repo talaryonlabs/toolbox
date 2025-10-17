7124ae98f11c839d52de0e85e9f342d5bacb821a
ferociousbyte <ferociousbyte@talaryonlabs.com>
2025-10-17 14:58:20 +0200

    Fix `AuthentikGroup` `Attributes` default initialization
    - Replaced default empty array with an instance of `Dictionary<string, object>` for proper dictionary initialization.

--- src/Toolbox/Services/Authentik/Models/AuthentikGroup.cs
+++ src/Toolbox/Services/Authentik/Models/AuthentikGroup.cs
@@ -8,5 +8,5 @@ public class AuthentikGroup
     [JsonProperty("num_pk")] public required string NumericGroupUuid { get; set; }
     [JsonProperty("name")] public required string Name { get; set; }
     [JsonProperty("is_superuser")] public bool IsSuperUser { get; set; }
-    [JsonProperty("attributes")] public IDictionary<string, object> Attributes { get; set; } = [];
+    [JsonProperty("attributes")] public IDictionary<string, object> Attributes { get; set; } = new Dictionary<string, object>();
 }
