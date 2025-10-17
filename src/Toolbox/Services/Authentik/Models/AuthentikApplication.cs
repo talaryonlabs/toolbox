0dbf297ddfad02b23ce861dd7764fb0c305a721b
ferociousbyte <ferociousbyte@talaryonlabs.com>
2025-10-17 15:20:56 +0200

    Map `AuthentikApplication` and `AuthentikUser` to JSON properties using `Newtonsoft.Json`

--- src/Toolbox/Services/Authentik/Models/AuthentikApplication.cs
+++ src/Toolbox/Services/Authentik/Models/AuthentikApplication.cs
@@ -1,6 +1,17 @@
-﻿namespace Talaryon.Toolbox.Services.Authentik.Models;
+﻿using Newtonsoft.Json;
+
+namespace Talaryon.Toolbox.Services.Authentik.Models;
 
 public class AuthentikApplication
 {
+    [JsonProperty("pk")] public required string Uuid { get; set; }
+    [JsonProperty("name")] public required string Name { get; set; }
+    [JsonProperty("slug")] public required string Slug { get; set; }
     
+    [JsonProperty("launch_url")] public string? LaunchUrl { get; set; }
+    [JsonProperty("group")] public string? Group { get; set; }
+    
+    [JsonProperty("meta_icon")] public string? MetaIcon { get; set; }
+    [JsonProperty("meta_description")] public string? MetaDescription { get; set; }
+    [JsonProperty("meta_publisher")] public string? MetaPublisher { get; set; }
 }
--- src/Toolbox/Services/Authentik/Models/AuthentikUser.cs
+++ src/Toolbox/Services/Authentik/Models/AuthentikUser.cs
@@ -1,6 +1,27 @@
-﻿namespace Talaryon.Toolbox.Services.Authentik.Models;
+﻿using Newtonsoft.Json;
+
+namespace Talaryon.Toolbox.Services.Authentik.Models;
 
 public class AuthentikUser
 {
+    [JsonProperty("pk")] public required string Uuid { get; set; }
+    [JsonProperty("username")] public required string Username { get; set; }
+    [JsonProperty("email")] public string? Email { get; set; }
+    [JsonProperty("name")] public required string Name { get; set; }
     
+    [JsonProperty("first_name")] public string? FirstName { get; set; }
+    [JsonProperty("last_name")] public string? LastName { get; set; }
+    
+    [JsonProperty("is_superuser")] public bool IsSuperUser { get; set; }
+    [JsonProperty("is_active")] public bool IsActive { get; set; }
+    
+    [JsonProperty("groups")] public string[] Groups { get; set; } = [];
+    [JsonProperty("avatar")] public string? Avatar { get; set; }
+    [JsonProperty("attributes")] public IDictionary<string, object> Attributes { get; set; } = new Dictionary<string, object>();
+    [JsonProperty("uid")] public required string Uid { get; set; }
+    [JsonProperty("path")] public string? Path { get; set; }
+
+    [JsonProperty("date_joined")] public DateTime DateJoined { get; set; }
+    [JsonProperty("last_login")] public DateTime LastLogin { get; set; }
+    [JsonProperty("last_updated")] public DateTime LastUpdated { get; set; }
 }
