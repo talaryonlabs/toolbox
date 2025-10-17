d8b4040c3088076689fed37eabb7461d55e530c8
ferociousbyte <ferociousbyte@talaryonlabs.com>
2025-10-17 18:19:16 +0200

    Add Authentik service integration and configuration
    Introduced the Authentik service with scoped dependency injection, configuration options, and HTTP client setup. Implemented a `Test` method in `IAuthentik` for basic functionality validation and created supporting models and utilities for API communication. Updated the Website project to use the new Authentik service.

--- src/Toolbox/Services/Authentik/Authentik.cs
+++ src/Toolbox/Services/Authentik/Authentik.cs
@@ -1,6 +1,51 @@
-﻿namespace Talaryon.Toolbox.Services.Authentik;
+﻿using System.Net.Http.Json;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Talaryon.Toolbox.Services.Authentik.Models;
 
-public class Authentik
+namespace Talaryon.Toolbox.Services.Authentik;
+
+public sealed class AuthentikOptions : TalaryonOptions<AuthentikOptions>
 {
+    public string? BaseUrl { get; set; }
+    public string? AccessToken { get; set; }
+}
+
+public class Authentik : IAuthentik
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _base;
     
+    public Authentik(HttpClient httpClient, IOptions<AuthentikOptions> optionsAccessor)
+    {
+        ArgumentNullException.ThrowIfNull(optionsAccessor);
+
+        _base = optionsAccessor.Value.BaseUrl ?? throw new ArgumentNullException(nameof(optionsAccessor.Value.BaseUrl));
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri(_base);
+        _httpClient.DefaultRequestHeaders.Add("Authorization",
+            $"Bearer {optionsAccessor.Value.AccessToken ?? throw new ArgumentNullException(optionsAccessor.Value.AccessToken)}");
+    }
+
+
+    public ITalaryonRunner Test()
+    {
+        return new AuthentikTester(_httpClient);
+    }
+}
+
+public class AuthentikResult<T>
+{
+    [JsonProperty("results")] public T[] Result { get; set; } = [];
+    [JsonProperty("pagination")] public object Pagination { get; set; }
+}
+
+public class AuthentikTester(HttpClient httpClient) : ITalaryonRunner
+{
+    public void Run() => RunAsync().RunSynchronously();
+
+    public async Task RunAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await httpClient.GetFromJsonAsync<AuthentikResult<AuthentikApplication>>("/core/applications/", cancellationToken);
+    }
 }
