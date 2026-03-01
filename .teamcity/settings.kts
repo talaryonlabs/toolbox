c5c7b738b1d5738d5d369ea1fb6c00d81dede022
ferociousbyte <ferociousbyte@TeamCity>
2026-03-01 22:37:11 +0000

    runners of 'Toolbox.Hosting' build configuration were updated (TeamCity change in 'Libraries / Toolbox' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -230,8 +230,8 @@ object ToolboxHosting : BuildType({
         dotnetRestore {
             name = "Restore Packages"
             id = "Restore_Packages"
-            projects = "src/Toolbox.Hosting/Toolbox.Hosting.csproj"
-            sources = "https://nuget.pkg.talaryon.dev/v3/index.json"
+            projects = "src/%local.projectName%/%local.projectName%.csproj"
+            sources = "https://%nuget.source.talaryon%/v3/index.json"
         }
         dotnetBuild {
             name = "Build"
