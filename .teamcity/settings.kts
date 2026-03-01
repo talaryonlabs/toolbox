dda560ad40e5d89c53b83c09af7dfd4871de1a99
ferociousbyte <ferociousbyte@TeamCity>
2026-03-01 22:26:05 +0000

    runners of 'Toolbox' build configuration were updated (TeamCity change in 'Libraries / Toolbox' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -151,7 +151,7 @@ object Toolbox : BuildType({
         dotnetNugetPush {
             name = "Push NuGet Package"
             id = "Push_NuGet_Package"
-            packages = "publish/*Toolbox*.nupkg"
+            packages = "publish/%env.package%.%env.version%.nupkg"
             serverUrl = "https://%nuget.source.talaryon%/v3/index.json"
             apiKey = "credentialsJSON:56baad1f-80c9-4e5e-8ad3-d684ac95dfb8"
         }
