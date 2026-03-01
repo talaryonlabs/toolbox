c87dc3831d882e69c0b2f138c0cb033f38dd0d07
ferociousbyte <ferociousbyte@TeamCity>
2026-03-01 21:16:20 +0000

    runners of 'Toolbox.Hosting' build configuration were updated (TeamCity change in 'Libraries / Toolbox' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -202,7 +202,7 @@ object ToolboxHosting : BuildType({
         dotnetNugetPush {
             name = "Push NuGet Package"
             id = "Push_NuGet_Package"
-            packages = "publish/*Toolbox*.nupkg"
+            packages = "publish/*Toolbox.Hosting*.nupkg"
             serverUrl = "https://nuget.pkg.talaryon.dev/v3/index.json"
             apiKey = "credentialsJSON:56baad1f-80c9-4e5e-8ad3-d684ac95dfb8"
         }
